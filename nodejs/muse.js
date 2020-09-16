142dca08144b774b1b82279c863eee60f74886ce
brian4462 <brian4462@gmail.com>
2020-09-16 15:17:26 +0900

    study npm package and modules 2020-09-16 15:17

---
+++ nodejs/muse.js
@@ -0,0 +1,13 @@
+// var M = {
+//   v:'v',
+//   f:function(){
+//     console.log(this.v);
+//   }
+// }
+//
+// M.f();
+
+// ./는 현재디렉토리
+var part = require('./mpart.js');
+console.log(part);
+part.f();
--- template.js
+++
@@ -1,8 +0,0 @@
-var name = 'Taewook';
-var letter = 'Dear ' + name +'\n\n 하이하이하잏아ㅣ';
-
-var letter = `Dear ${name}
-
-하이하이하잏아ㅣ test ${1+1}`;
-
-console.log(letter);
