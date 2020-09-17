a35db83cf833fc51d7cef0f2323df5faa2513aa0
brian4462 <brian4462@gmail.com>
2020-09-17 16:04:56 +0900

    main.js code refactoring

--- main.js
+++ main.js
@@ -1,19 +1,9 @@
 var http = require('http');
-var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
-var path = require('path');
-var sanitizeHtml = require('sanitize-html');
-var mysql = require('mysql2');
-var db = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'kim25134460*',
-  database : 'opentutorials'
-});
-
-db.connect();
+var db = require('./lib/db');
+var topic = require('./lib/topic');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -21,152 +11,20 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/'){
       if(queryData.id === undefined){
-        db.query(`SELECT * FROM topic`, function(error,topics,fields){
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = template.list(topics);
-          var html = template.html(title, list,
-              `<h2>${title}</h2>${description}`,
-              `<div id="btnArea"><a href="/create">create</a></div></ol>`
-          );
-          response.writeHead(200);
-          response.end(html);
-        });
+        topic.home(request, response);
       } else {
-        db.query(`SELECT * FROM topic`, function(error,topics){
-          if(error){
-            throw error;
-          }
-          db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id], function(error2,topic){
-            if(error2){
-              throw error2;
-            }
-            var title = topic[0].title;
-            var description = topic[0].description;
-            var list = template.list(topics);
-            var html = template.html(title, list,
-                `<h2>${title}</h2>${description}`,
-                `<div id="btnArea"><a href="/create">create</a>
-                <a href="/update?id=${queryData.id}">update</a>
-                <form action="delete_process" method="post">
-                <input type="hidden" name="id" value="${queryData.id}">
-                <input type="submit" value="delete">
-                </form></div></ol>
-                `
-            );
-            response.writeHead(200);
-            response.end(html);
-          });
-        });
-
+        topic.page(request, response);
       }
     } else if(pathname === '/create'){
-      db.query(`SELECT * FROM topic`, function(error,topics,fields){
-        if(error){
-          throw error;
-        }
-        var title = 'WEB - create';
-        var list = template.list(topics);
-        var html = template.html(title, list,
-          `
-          <form action="/create_process" method="post">
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p>
-              <textarea name="description" placeholder="description"></textarea>
-            </p>
-            <p>
-              <input type="submit">
-            </p>
-          </form>
-          `,'</ol>');
-        response.writeHead(200);
-        response.end(html);
-      });
+      topic.create(request, response);
     } else if(pathname === '/create_process'){
-      var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-          var post = qs.parse(body);
-          db.query(`
-            INSERT INTO topic(title,description,created,author_id) VALUES(?,?,NOW(),?)`,
-          [post.title, post.description, 1],
-          function(error, result){
-            if(error){
-              throw error;
-            }
-            response.writeHead(302, {Location: `/?id=${result.insertId}`});
-            response.end();
-          });
-      });
+      topic.create_process(request,response);
     } else if(pathname === '/update'){
-      db.query(`SELECT * FROM topic`, function(error,topics,fields){
-        if(error){
-          throw error;
-        }
-        db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id], function(error2,topic){
-          if(error2){
-            throw error2;
-          }
-          var title = topic[0].title;
-          var description = topic[0].description;
-          var list = template.list(topics);
-          var html = template.html(title, list,
-            `
-            <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${topic[0].id}">
-              <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
-              <p>
-                <textarea name="description" placeholder="description">${topic[0].description}</textarea>
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `
-            ,'</ol>'
-          );
-          response.writeHead(200);
-          response.end(html);
-        });
-      });
+      topic.update(request,response);
     } else if(pathname === '/update_process'){
-      var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-        var post = qs.parse(body);
-        db.query(
-          `UPDATE topic SET title=?, description=?, author_id=? WHERE id=?`,
-          [post.title, post.description, 1, post.id],
-          function(error, result){
-            if(error){
-              throw error;
-            }
-          response.writeHead(302, {Location: `/?id=${post.id}`});
-          response.end();
-          }
-        );
-      });
+      topic.update_process(request,response);
     } else if(pathname === '/delete_process'){
-      var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-          var post = qs.parse(body);
-          var id = post.id;
-          var filterdId = path.parse(id).base;
-          db.query('DELETE FROM topic WHERE id = ?', [post.id], function(error, result){
-            if(error){
-              throw error;
-            }
-            response.writeHead(302, {Location: `/`});
-            response.end();
-          });
-      });
+      topic.delete_process(request,response);
     } else {
       response.writeHead(404);
       response.end('Not found');
