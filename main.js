fa2f8bdd3337adf50ea50a9c6cf99bba5db3a2e4
brian4462 <brian4462@gmail.com>
2020-09-18 10:07:13 +0900

    미들웨어 적용

--- main.js
+++ main.js
@@ -8,58 +8,49 @@ var template = require('./lib/template.js');
 var topic = require('./lib/topic');
 var author = require('./lib/author');
 
+//미들웨어
+var bodyParser = require('body-parser');
+var compression = require('compression');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(compression());
+
 //route, routing 길을따라 가다가 갈림길에서 적당한곳으로 방향을 잡는것
 app.get('/', function(request, response){
   topic.home(request, response);
 });
-
 app.get('/page/:pageId', function(request, response){
   topic.page(request, response);
 });
+app.get('/create', function(request, response){
+  topic.create(request, response);
+});
+app.post('/create_process', function(request, response){
+  topic.create_process(request, response);
+});
+app.get('/update/:pageId', function(request, response){
+  topic.update(request,response);
+});
+app.post('/update_process', function(request, response){
+  topic.update_process(request, response);
+});
+app.post('/delete_process', function(request, response){
+  topic.delete_process(request, response);
+});
+//author
+app.get('/author', function(request, response){
+  author.home(request,response);
+});
+app.post('/author/create_process', function(request, response){
+  author.create_process(request, response);
+});
+app.get('/author/update/:authorId', function(request, response){
+  author.update(request,response);
+});
+app.post('/author/update_process', function(request, response){
+  author.update_process(request, response);
+});
+app.post('/author/delete_process', function(request, response){
+  author.delete_process(request, response);
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
-
-// var http = require('http');
-// var url = require('url');
-// var qs = require('querystring');
-// var template = require('./lib/template.js');
-// var db = require('./lib/db');
-// var topic = require('./lib/topic');
-// var author = require('./lib/author');
-//
-// var app = http.createServer(function(request,response){
-//     var _url = request.url;
-//     var queryData = url.parse(_url, true).query;
-//     var pathname = url.parse(_url, true).pathname;
-//     if(pathname === '/'){
-//       if(queryData.id === undefined){
-//         topic.home(request, response);
-//       } else {
-//         topic.page(request, response);
-//       }
-//     } else if(pathname === '/create'){
-//       topic.create(request, response);
-//     } else if(pathname === '/create_process'){
-//       topic.create_process(request,response);
-//     } else if(pathname === '/update'){
-//       topic.update(request,response);
-//     } else if(pathname === '/update_process'){
-//       topic.update_process(request,response);
-//     } else if(pathname === '/delete_process'){
-//       topic.delete_process(request,response);
-//     } else if(pathname === '/author'){
-//       author.home(request, response);
-//     } else if(pathname === '/author/create_process'){
-//       author.create_process(request, response);
-//     } else if(pathname === '/author/update'){
-//       author.update(request, response);
-//     } else if(pathname === '/author/update_process'){
-//       author.update_process(request, response);
-//     } else if(pathname === '/author/delete_process'){
-//       author.delete_process(request,response);
-//     } else {
-//       response.writeHead(404);
-//       response.end('Not found');
-//     }
-// });
-// app.listen(3000);
+app.listen(3000, () => console.log('main.js start success on 3000!'));
